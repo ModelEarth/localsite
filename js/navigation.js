bddfe5b31ef29b7f12758f6b723cf272ace6be28
modelearth <model@dreamstudio.com>
2021-12-04 10:34:56 -0500

    Move changFavicon

--- js/navigation.js
+++ js/navigation.js
@@ -25,6 +25,21 @@ if (climbpath == "") {
 //console.log("climbpath " + climbpath);
 let earthFooter = false;
 
+const changeFavicon = link => {
+  let $favicon = document.querySelector('link[rel="icon"]')
+  // If a <link rel="icon"> element already exists,
+  // change its href to the given link.
+  if ($favicon !== null) {
+    $favicon.href = link
+  // Otherwise, create a new element and append it to <head>.
+  } else {
+    $favicon = document.createElement("link")
+    $favicon.rel = "icon"
+    $favicon.href = link
+    document.head.appendChild($favicon)
+  }
+}
+
 $(document).ready(function(){
 	
  	var modelpath = climbpath;
@@ -689,17 +704,4 @@ function getPageFolder(pagePath) {
   }
   return pageFolder;
 }
-const changeFavicon = link => {
-  let $favicon = document.querySelector('link[rel="icon"]')
-  // If a <link rel="icon"> element already exists,
-  // change its href to the given link.
-  if ($favicon !== null) {
-    $favicon.href = link
-  // Otherwise, create a new element and append it to <head>.
-  } else {
-    $favicon = document.createElement("link")
-    $favicon.rel = "icon"
-    $favicon.href = link
-    document.head.appendChild($favicon)
-  }
-}
+
