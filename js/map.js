c751f4ccbb92818d7af23d3ac599705f02c53d7e
modelearth <model@dreamstudio.com>
2023-07-09 13:56:33 -0400

    Move

--- js/map.js
+++ js/map.js
@@ -428,7 +428,7 @@ function loadMap1(calledBy, show, dp_incoming) { // Called by this page. Maybe s
         //alert("wastewater4")
         dp.listTitle = "Georgia Wastewater Facilities (2023)";
         dp.editLink = "https://docs.google.com/spreadsheets/d/1YmfBPEFpfmaKmxcnxijPU8-esVkhaVBE1wLZqPNOKtY/edit?usp=sharing";
-        dp.listInfo = "Post comments in our <a href='https://docs.google.com/spreadsheets/d/1YmfBPEFpfmaKmxcnxijPU8-esVkhaVBE1wLZqPNOKtY/edit?usp=sharing' target='georgia_recyclers_sheet'>Google&nbsp;Sheet</a> to provide updates.<br>View&nbsp;<a href='../map/recycling/ga/'>Solid Waste and Recycling&nbsp;Datasets</a>.";
+        dp.listInfo = "View&nbsp;<a href='../map/recycling/ga/'>Solid Waste and Recycling&nbsp;Datasets</a>.";
         // Wastewater tab. Path including gid from sheet's Share > Publish [choose tab]
         dp.googleCSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRBRXb005Plt3mmmJunBMk6IejMu-VAJOPdlHWXUpyecTAF-SK4OpfSjPHNMN_KAePShbNsiOo2hZzt/pub?gid=2016874057&single=true&output=csv";
         
@@ -2077,6 +2077,7 @@ function showList(dp,map) {
       if (hash.subcat) {
         listTitle += hash.subcat;
       }
+      //listTitle = "title"; // name;
       $(".listTitle").html(listTitle);
 
       if ($("#catSearch").val() || hash.cat || hash.subcat) {
