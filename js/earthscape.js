e141d55a7a9d3b7b8c758e9497a95061ed3ce7db
modelearth <model@dreamstudio.com>
2024-03-16 22:36:17 -0400

    maxHeight:"300px"

--- js/earthscape.js
+++ js/earthscape.js
@@ -26,7 +26,7 @@ function earthscape_TableDisplay(my,data) {
         //layout:"fitColumns",      //fit columns to width of table
         //responsiveLayout:"hide",  //hide columns that dont fit on the table
         //tooltips:true,          //show tool tips on cells
-        maxHeight:"500px",        // For frozenRows
+        maxHeight:"300px",        // For frozenRows
         addRowPos:"top",          //when adding a new row, add it to the top of the table
         history:true,             //allow undo and redo actions on the table
         paginationSize:7,         //allow 7 rows per page of data
